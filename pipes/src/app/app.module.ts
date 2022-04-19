072f6f83bc5de25f2108d66c34139ca16c597899
vikas <v8kgsm@gmail.com>
2022-04-19 19:31:43 +0530

    pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShortenPipe,
+    FilterPipe
+  ],
+  imports: [
+      BrowserModule,
+      FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
